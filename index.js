35a724e9eabc639e25781430c1bc75c78b0459ba
Thanwarat Saengsai <69625706+badman4k-png@users.noreply.github.com>
2022-05-03 02:47:02 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressSession = require("express-session");
 const app = express();
 require('dotenv').config()
 
@@ -8,12 +9,17 @@ const db = require('./ConnectDB/datdabase');
 const route = require('./routers/route');
 
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(route);
+app.use(expressSession({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { }
+})); 
 app.use(express.static(__dirname + '/www'));
-
-
-app.listen(3000,() =>{
-  console.log('server is running');
+app.use('/api',require('./routers/route'))
+app.listen(3000, () => {
+  console.log('server is running on Port 3000');
 })
