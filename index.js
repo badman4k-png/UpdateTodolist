2230ffb61604f8ba68522febfb369e743450bca5
= <=>
2022-05-03 14:02:38 +0700

    update2.0

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const expressSession = require("express-session");
 const app = express();
@@ -19,35 +18,9 @@ app.use(expressSession({
   saveUninitialized: true,
   cookie: { }
 })); 
+app.use('./api', require('./routers/route'))
 app.use(express.static(__dirname + '/www'));
-app.use('/api',require('./routers/route'))
-app.listen(3000, () => {
-  console.log('server is running on Port 3000');
-})
-=======
-const express = require('express');
-const expressSession = require("express-session");
-const app = express();
-require('dotenv').config()
- 
-const bodyparser = require('body-parser');
-const cors = require('cors');
-const db = require('./ConnectDB/datdabase');
-const route = require('./routers/route');
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(route);
-app.use(expressSession({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { }
-})); 
-app.use(express.static(__dirname + '/www'));
-app.use('/api',require('./routers/route'))
 app.listen(3000, () => {
-  console.log('server is running on Port 3000');
+  console.log('server is running');
 })
->>>>>>> 4d3acc7379925808a7fdf2c53b279b16e846718e
--- models/member.js
+++ models/member.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Collection } = require('mongodb');
 const mongoose = require('mongoose');
 const collection = 'Data_Register';
@@ -13,22 +12,4 @@ const memberSchema = mongoose.Schema({
     versionKey: false,
     collection
 })
-
-=======
-const { Collection } = require('mongodb');
-const mongoose = require('mongoose');
-const collection = 'Data_Register';
-const memberSchema = mongoose.Schema({
-    Username: String,
-    name: String,
-    email: String,
-    password: String,
-    phone: String
-},{
-    timestamps: true,
-    versionKey: false,
-    collection
-})
-
->>>>>>> 4d3acc7379925808a7fdf2c53b279b16e846718e
 module.exports = mongoose.model(collection,memberSchema);
